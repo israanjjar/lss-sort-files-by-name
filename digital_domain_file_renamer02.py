871693a33fcba57e6cdb9480c4f25d0811db5057
Israa Elnjjar <israa.njjar@gmail.com>
2015-05-02 14:18:13 -0700

    after installing  PyCharm I refactored the code according to PyCharm suggestions

--- digital_domain_file_renamer02.py
+++ digital_domain_file_renamer02.py
@@ -1,122 +1,149 @@
 #!usr/bin/python
-import sys, os
-import re, itertools, difflib, operator
+import sys
+import os
+import re
+import itertools
+import operator
 
 
 def open_directory(path):
-	#open the file and get all the file names
-	filelist = [os.path.normcase(f) for f in os.listdir(path)]
-	return filelist
+    # open the file and get all the file names
+    """
+
+    :rtype : list
+    """
+    file_list = [os.path.normcase(f) for f in os.listdir(path)]
+    return file_list
 
 
-def regex_filename(filelist):
-	# print filelist
-	result = {}
-	#copy the array first
-	for index, filename in enumerate(filelist):
-		if index < len(filelist) -1 :
-			#run a regex on filenames, return an array of [number, len, start, end] 
-			current_file = regex_filename_results(filename)
-			after_file = regex_filename_results(filelist[index+1])
-			if current_file == []: result[filename] = [[filename]]
-			result = add_replaced(current_file, after_file, index, result, filelist, filename)
-	return result
+def regex_filename(file_list):
+    # print file list
+    result = {}
+    # copy the array first
+    for index, filename in enumerate(file_list):
+        if index < len(file_list) - 1:
+            # run a regex on file names, return an array of [number, len, start, end]
+            current_file = regex_filename_results(filename)
+            after_file = regex_filename_results(file_list[index + 1])
+            if not current_file:
+                result[filename] = [[filename]]
+            result = add_replaced(current_file, after_file, index, result, file_list, filename)
+    return result
 
 
-def add_replaced(current_file, after_file, index, result, filelist, filename):
-	if len(after_file) > 0 and len(current_file) > 0:
-		# print current_file
-		for ind, regexdnumberlist in enumerate(current_file):
-				#replace the file name the c style fprint formatting 
-			replaced_current = replace_filename(filelist[index], regexdnumberlist[0], regexdnumberlist[1])
-			replaced_after = replace_filename(filelist[index+1], after_file[ind][0], after_file[ind][1])
-			
-			#if the current element and the one after equal, add current to result
-			if replaced_after == replaced_current: 
-				result = match(result, replaced_current, replaced_after, regexdnumberlist, after_file, ind, filelist, index)
-			else: 
-				result = no_matches(result, index, ind, filelist, replaced_current, regexdnumberlist)
-				if len(current_file) == 1 and not (result.has_key(replaced_current)) : result[filename] = [filename]
-			#what if they are == ? Compare it to the one before it. 
-	return result
+def add_replaced(current_file, after_file, index, result, file_list, filename):
+    if len(after_file) > 0 and len(current_file) > 0:
+        # print current_file
+        for ind, regexd_number_list in enumerate(current_file):
+            # replace the file name the c style f print formatting
+            replaced_current = replace_filename(file_list[index], regexd_number_list[0], regexd_number_list[1])
+            replaced_after = replace_filename(file_list[index + 1], after_file[ind][0], after_file[ind][1])
+
+            # if the current element and the one after equal, add current to result
+            if replaced_after == replaced_current:
+                result = match(result, replaced_current, replaced_after, regexd_number_list, after_file, ind, file_list,
+                               index)
+            else:
+                result = no_matches(result, index, ind, file_list, replaced_current, regexd_number_list)
+                if 1 == len(current_file) and replaced_current not in result:
+                    # what if they are == ? Compare it to the one before it.
+                    result[filename] = [filename]
+    return result
 
 
-def replace_filename(filename, numbers, replacewith):
-	cformatreplace = replace_printf(replacewith)
-	return filename.replace(str(numbers), cformatreplace)
+def replace_filename(filename, numbers, replace_with):
+    """
 
 
-def no_matches(result, index, ind, filelist, replaced_current, regexdnumberlist):
-	if index-1 > -1: 
-		before_file = regex_filename_results(filelist[index-1])
-		if len(before_file) >0: 
-			replaced_before = replace_filename(filelist[index-1], before_file[ind][0], before_file[ind][1])
-			if replaced_current == replaced_before: result[replaced_current].append([replaced_current, regexdnumberlist])
-	return result
+    :param filename: str
+    :param numbers: numbers in the string
+    :param replace_with: c style formatting string
+    :rtype : str
+    """
+    cformatreplace = replace_printf(replace_with)
+    return filename.replace(str(numbers), cformatreplace)
+
+
+def no_matches(result, index, ind, file_list, replaced_current, regexdnumberlist):
+    if index - 1 > -1:
+        before_file = regex_filename_results(file_list[index - 1])
+        if len(before_file) > 0:
+            replaced_before = replace_filename(file_list[index - 1], before_file[ind][0], before_file[ind][1])
+            if replaced_current == replaced_before:
+                result[replaced_current].append([replaced_current, regexdnumberlist])
+    assert isinstance(result, dict)
+    return result
 
 
 def match(result, replaced_current, replaced_after, regexdnumberlist, after_file, ind, filelist, index):
-	if not result.has_key(replaced_current): result[replaced_current] = []
-	result[replaced_current].append([replaced_current, regexdnumberlist])
-	#only for the last element: 
-	#if this is the last item, we know already that it's equal lets add it then. 
-	if index == len(filelist) -2: 
-		if not result.has_key(replaced_after): result[replaced_after] = []
-		result[replaced_after].append([replaced_after, after_file[ind]])
-	return result
+    if replaced_current not in result:
+        result[replaced_current] = []
+    result[replaced_current].append([replaced_current, regexdnumberlist])
+    # only for the last element:
+    # if this is the last item, we know already that it's equal lets add it then.
+    if index == len(filelist) - 2:
+        if replaced_after not in result:
+            result[replaced_after] = []
+        result[replaced_after].append([replaced_after, after_file[ind]])
+    return result
 
 
 def print_results(hash_regxed_files):
-	#print the results!
-	for key in hash_regxed_files:
-		filerange = []
-		count = 0 
-		for eachgroup in hash_regxed_files[key]:
-			if len(eachgroup) >1: filerange.append(eachgroup[1][0])
-			count = count + 1 
-		if count >1:
-			file_range = files_range(filerange)
-		else:
-			file_range = ""
-		print("%s  %s                  %s" % (count, key, file_range))
+    # print the results!
+    for key in hash_regxed_files:
+        file_range = []
+        count = 0
+        for each_group in hash_regxed_files[key]:
+            if len(each_group) > 1:
+                file_range.append(each_group[1][0])
+            count += 1
+        if count > 1:
+            file_range = files_range(file_range)
+        else:
+            file_range = ""
+        print("%s  %s                  %s" % (count, key, file_range))
 
 
 def files_range(numbers):
-	rangelist = ""
-	for k, g in itertools.groupby(enumerate(numbers), lambda (i,x):int(i)-int(x)):
-		result = map(operator.itemgetter(1), g)
-		rangelist += ("%s - %s   " % (result[0], result[len(result)-1]) )
-	return rangelist
+    range_list = ""
+    for k, g in itertools.groupby(enumerate(numbers), lambda (i, x): int(i) - int(x)):
+        result = map(operator.itemgetter(1), g)
+        range_list += ("%s - %s   " % (result[0], result[len(result) - 1]))
+    return range_list
 
 
-def replace_printf(number): 
-	if number > 2:
-		return "%0"+str(number)+"d"
-	else:
-		return "%d"
+def replace_printf(number):
+    if number > 2:
+        return "%0" + str(number) + "d"
+    else:
+        return "%d"
 
 
 def regex_filename_results(filename):
-	#let's get all the numbers
-	result =[]
-	for m in re.finditer(r"\d+", filename):
-		numberlength = m.end() - m.start()
-		result.append([m.group(0), numberlength, m.start(), m.end()])
-	return result
+    # let's get all the numbers
+    """
+
+    :rtype : dict
+    """
+    result = []
+    for m in re.finditer(r"\d+", filename):
+        number_length = m.end() - m.start()
+        result.append([m.group(0), number_length, m.start(), m.end()])
+    return result
 
 
 def run():
-	#Where all the magic happens
-	if len(sys.argv) > 1 :
-		filepath = str(sys.argv[1])
-	else :
-		filepath = './'
-	filelist = open_directory(filepath)
-	result = regex_filename(filelist)
-	print_results(result)
+    # Where all the magic happens
+    if len(sys.argv) > 1:
+        file_path = str(sys.argv[1])
+    else:
+        file_path = './'
+    file_list = open_directory(file_path)
+    result = regex_filename(file_list)
+    print_results(result)
 
 
-#now run the code, 
+# now run the code,
 run()
 
 
