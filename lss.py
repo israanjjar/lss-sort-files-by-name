c5c3b991d51ea41d70685ca94647f0581c79da9e
Israa Elnjjar <israa.njjar@gmail.com>
2015-05-05 14:39:55 -0700

    added comments

--- lss.py
+++ lss.py
@@ -7,16 +7,13 @@ import operator
 
 
 def open_directory(path):
-    # 
-    """
-    open the file from the given path retrun a list of all the files names
-    :rtype : list
-    """
+    """ open the file from the given path return a list of all the files names :rtype : list """
     file_list = [os.path.normcase(f) for f in os.listdir(path)]
     return file_list
 
 
 def regex_filename(file_list):
+    """ if the files had no number then add them the the dic already, else run add_replaced """
     result = {}
     # copy the array first
     for index, filename in enumerate(file_list):
@@ -31,6 +28,7 @@ def regex_filename(file_list):
 
 
 def add_replaced(current_file, after_file, index, result, file_list, filename):
+    """compare the files and replace them by calling helper methods """
     if len(after_file) > 0 and len(current_file) > 0:
         # print current_file
         for ind, regexd_number_list in enumerate(current_file):
@@ -51,9 +49,7 @@ def add_replaced(current_file, after_file, index, result, file_list, filename):
 
 
 def replace_filename(filename, numbers, replace_with):
-    """
-    replace the numbers in filename with a c style formatting 
-    example: file01_0040.rgb => file01_%04d.rgb 
+    """ replace the numbers in filename with a c style formatting  example: file01_0040.rgb => file01_%04d.rgb
 
     :param filename: str File name before formatting 
     :param numbers: str numbers in the string
@@ -64,16 +60,37 @@ def replace_filename(filename, numbers, replace_with):
     return filename.replace(str(numbers), cformatreplace)
 
 
+def match(result, replaced_current, replaced_after, regexdnumberlist, after_file, ind, file_list, index):
+    """  Add to dict if matched.
+    :param result: dict File name before formatting
+    :param index: int index of file_names
+    :param ind: int index of current numbers in a file name array
+    :param file_list: list of all the files names
+    :param replaced_after: str the file name after replacing
+    :param regexdnumberlist: list of the numbers in a filename with len, start, and end ['03', 2, 27, 29]
+    :rtype : dict
+    """
+    if replaced_current not in result:
+        result[replaced_current] = []
+    result[replaced_current].append([replaced_current, regexdnumberlist])
+    # only for the last element:
+    # if this is the last item, we know already that it's equal lets add it then.
+    if index == len(file_list) - 2:
+        if replaced_after not in result:
+            result[replaced_after] = []
+        result[replaced_after].append([replaced_after, after_file[ind]])
+    return result
+
+
 def no_matches(result, index, ind, file_list, replaced_current, regexdnumberlist):
     """
-    replace the numbers in filename with a c style formatting
-    example: file01_0040.rgb => file01_%04d.rgb
-
+    if there are no matches then take the one before and compare it to current if the index is not 0
     :param result: dict File name before formatting
-    :param index: int numbers in the string
-    :param file_list: list c style formatting string
-    :param replaced_current: str c style formatting string
-    :param regexdnumberlist: list c style formatting string
+    :param index: int index of file_names
+    :param ind: int index of current numbers in a file name array
+    :param file_list: list of all the files names
+    :param replaced_current: str the file name after replacing
+    :param regexdnumberlist: list of the numbers in a filename with len, start, and end ['03', 2, 27, 29]
     :rtype : dict
     """
     if index - 1 > -1:
@@ -82,25 +99,11 @@ def no_matches(result, index, ind, file_list, replaced_current, regexdnumberlist
             replaced_before = replace_filename(file_list[index - 1], before_file[ind][0], before_file[ind][1])
             if replaced_current == replaced_before:
                 result[replaced_current].append([replaced_current, regexdnumberlist])
-    assert isinstance(result, dict)
-    return result
-
-
-def match(result, replaced_current, replaced_after, regexdnumberlist, after_file, ind, filelist, index):
-    if replaced_current not in result:
-        result[replaced_current] = []
-    result[replaced_current].append([replaced_current, regexdnumberlist])
-    # only for the last element:
-    # if this is the last item, we know already that it's equal lets add it then.
-    if index == len(filelist) - 2:
-        if replaced_after not in result:
-            result[replaced_after] = []
-        result[replaced_after].append([replaced_after, after_file[ind]])
     return result
 
 
 def print_results(hash_regxed_files):
-    # print the results!
+    """ print the result """
     for key in hash_regxed_files:
         file_range = []
         count = 0
@@ -112,11 +115,11 @@ def print_results(hash_regxed_files):
             file_range = files_range(file_range)
         else:
             file_range = ""
-        # print("%s\t%s%s" % (count, key, file_range))
         print '{0:<2} {1:>35} {2:>30}'.format(count, key, file_range)
 
 
 def files_range(numbers):
+    """ create the file ranges"""
     range_list = ""
     for k, g in itertools.groupby(enumerate(numbers), lambda (i, x): int(i) - int(x)):
         result = map(operator.itemgetter(1), g)
@@ -125,6 +128,7 @@ def files_range(numbers):
 
 
 def replace_printf(number):
+    """ replace numbers with %d """
     if number > 2:
         return "%0" + str(number) + "d"
     else:
@@ -132,11 +136,11 @@ def replace_printf(number):
 
 
 def regex_filename_results(filename):
-    # let's get all the numbers
-    """
-
-    :rtype : list
-    """
+    """  grabs the numbers from a file name and returns
+     :param filename: the name of teh file -> example = file01_0040.rgb
+     :rtype : list result  -> example = ['03', 2, 27, 29]
+     result[0] = numbers, list[1]= len(list[0]), list[2]= start index, list[3]= end index
+     """
     result = []
     for m in re.finditer(r"\d+", filename):
         number_length = m.end() - m.start()
